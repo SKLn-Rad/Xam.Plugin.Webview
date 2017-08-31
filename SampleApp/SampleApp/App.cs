674e3fed9f8efeee753444d46882190cbd7aaad8
Ryan Dixon <ryandixon1993@gmail.com>
2017-08-31 11:53:41 +0100

    1.7.0-rc1

--- SampleApp/SampleApp/App.cs
+++ SampleApp/SampleApp/App.cs
@@ -8,7 +8,7 @@ namespace SampleApp
 
         public App()
         {
-            MainPage = new HeadersSample();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
