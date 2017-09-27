efe5ea5617567f5f8ca2526fbecf16c787ddb25e
Ryan Dixon <ryandixon1993@gmail.com>
2017-09-27 14:39:51 +0100

    MacOS Sample
    Implement the MacOS sample app

---
+++ SampleApp/SampleApp.MacOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace SampleApp.MacOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "Xam.Plugin.FormsWebView";
+            _window.TitleVisibility = NSWindowTitleVisibility.Visible;
+        }
+
+        public override NSWindow MainWindow => _window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
