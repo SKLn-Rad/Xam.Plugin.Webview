0ffb07f98a1909f4517e1e375226526ff256d095
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2020-01-01 17:36:46 +0800

    fix issue

--- SampleApp/SampleApp.Droid/MainActivity.cs
+++ SampleApp/SampleApp.Droid/MainActivity.cs
@@ -13,7 +13,7 @@ using Xam.Plugin.WebView.Droid;
 namespace SampleApp.Droid
 {
     [Activity(Label = "SampleApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, IMainActivityWithStarting
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -26,6 +26,22 @@ namespace SampleApp.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        private Action<int, Result, Intent> _resultCallback;
+        public void StartActivity(Intent intent, int requestCode, Action<int, Result, Intent> resultCallback)
+        {
+            _resultCallback = resultCallback;
+            StartActivityForResult(intent, requestCode);
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (_resultCallback != null)
+            {
+                _resultCallback(requestCode, resultCode, data);
+                _resultCallback = null;
+            }
         }
     }
 }
---
+++ Xam.Plugin.WebView.Droid/IMainActivityWithStarting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Xam.Plugin.WebView.Droid
+{
+    public interface IMainActivityWithStarting
+    {
+        void StartActivity(Intent intent, int requestCode, Action<int, Result, Intent> resultCallback);
+    }
+}
